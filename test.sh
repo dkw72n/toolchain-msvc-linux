8709834ec4a741261f30639863cb312ebec97dd3
dkw72n <root@localhost>
2026-01-06 05:48:05 +0000

    added presets

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
-cmake -B build -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-msvc-linux.cmake -DWDKBASE=$HOME/winsdk/sdk -DMSVCBASE=$HOME/winsdk/crt
+#cmake -B build -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-msvc-linux.cmake -DWDKBASE=$HOME/winsdk/sdk -DMSVCBASE=$HOME/winsdk/crt
+#cmake --build build
 
-cmake --build build
+cmake --preset make-msvc-linux
+cmake --build --preset make
